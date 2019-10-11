088520b4315a46559a9f8e7237045dbaea3fd951
TheSlothinatoor <31484277+TheSlothinatoor@users.noreply.github.com>
2019-10-11 12:07:20 +0100

    Update
    Removed some code from the main file and put it into nik's test file

--- Nik.py
+++ Nik.py
@@ -10,5 +10,20 @@ async def on_ready():
 async def on_message(message):
     if message.content == "!Hi":
         await message.channel.send("Hello")
+    elif "!Purge" in message.content:
+        global numToDelete
+        numToDelete = ""
+        counter = 0
+        for character in message.content:
+            if character in "1234567890":
+                numToDelete = numToDelete + character
+            elif character == " ":
+                break
+            numToDelete = int(numToDelete)
+        async for oldMessage in Client.logs_from(ctx.message.channel, limit = numToDelete):
+            if counter < numToDelete:
+                await Client.delete_message(oldMessage)
+                counter = counter + 1
+                await syncio.sleep(1.2)
 
 client.run("NjMyMTMzMDA4MTc4MDIwMzUz.XaBAUQ.ySHJRsIiLUswtta8kXqRbZddXQ4")
