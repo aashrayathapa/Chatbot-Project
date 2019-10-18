c5f73132e7f406eff2e04a1d68d4934f9235a236
TheSlothinatoor <31484277+TheSlothinatoor@users.noreply.github.com>
2019-10-18 11:53:42 +0100

    Removed useless purge and delete
    Added the find, working on refining and filtering only the team

--- Nik.py
+++ Nik.py
@@ -1,6 +1,15 @@
 import discord
+import pyfootball
+import http.client
+import json
 
-client = discord.Client()
+client = discord.Client() #sets the discord client up
+
+connection = http.client.HTTPConnection("api.football-data.org")
+headers = { "X-Auth-Token": "49514fb34530408e95aba7f3b49733ad" }
+connection.request("GET", "/v2/competitions/DED", None, headers )
+response = json.loads(connection.getresponse().read().decode())
+print(response)
 
 @client.event
 async def on_ready():
@@ -10,24 +19,11 @@ async def on_ready():
 async def on_message(message):
     if "!HI" in message.content.upper():
         await message.channel.send("Hello")
-    elif "!PURGE" in message.content.upper():
-        global numToDelete
-        numToDelete = ""
-        counter = 0
-        for character in message.content:
-            if character in "1234567890":
-                numToDelete = numToDelete + character
-            elif character == " ":
-                break
-            numToDelete = int(numToDelete)
-        async for oldMessage in Client.logs_from(ctx.message.channel, limit = numToDelete):
-            if counter < numToDelete:
-                await Client.delete_message(oldMessage)
-                counter = counter + 1
-                await syncio.sleep(1.2)
-    elif "!DELETE" in message.content.upper():
-        tmp = await message.channel.send("Clearing...")
-        async for msg in client.logs_from(message.channel):
-            await client.delete_message(msg)
+    elif "FIND" in message.content.upper() and "TEAM" in message.content.upper():
+        for letter in message.content:
+            print(letter)
+        teamToSearch = message.content
+        teams = f.search_teams()
+        print(teams)
 
-client.run("NjMyMTMzMDA4MTc4MDIwMzUz.XaBAUQ.ySHJRsIiLUswtta8kXqRbZddXQ4")
+client.run("NjMyMTMzMDA4MTc4MDIwMzUz.XaSOeg.vmXqdHfduVBSW_voDMYCUjgUMmo")
