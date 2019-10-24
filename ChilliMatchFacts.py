9749d3ed8e81965c4cc5cd634a0b2e2e9dff01ab
ChillitheCode <56298476+ChillitheCode@users.noreply.github.com>
2019-10-24 14:50:00 +0100

    Update ChilliMatchFacts.py
    A token and relevant code has been added

--- ChilliMatchFacts.py
+++ ChilliMatchFacts.py
@@ -1,1 +1,33 @@
 import discord
+import pyfootball
+import http.client
+import json
+
+connection = http.client.HTTPConnection("api.football-data.org")
+headers = { "X-Auth-Token": "49514fb34530408e95aba7f3b49733ad" }
+connection.request("GET", "/v2/competitions/DED", None, headers )
+response = json.loads(connection.getresponse().read().decode())
+print(response)
+
+client = discord.Client()
+analyzer = SentimentIntensityAnalyzer()
+translator = Translator()
+
+@client.event
+async def on_ready():
+    print("We have logged in")
+
+@client.event
+async def on_message(message):
+    if "!HI" in message.content.upper():
+        await message.channel.send("Hello")
+    elif "FIND" in message.content.upper() and "TEAM" in message.content.upper(): #not completed yet
+        for letter in message.content:
+            print(letter) #testing the loop
+        teamToSearch = message.content
+        teams = f.search_teams()
+        print(teams
+
+on_message(message)
+
+client.run("NjMyMTMzMDA4MTc4MDIwMzUz.XaBAUQ.ySHJRsIiLUswtta8kXqRbZddXQ4")
