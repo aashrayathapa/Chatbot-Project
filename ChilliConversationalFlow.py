e2e9ba53ab320af5f90fd08cdb9a8a129414a193
ChillitheCode <56298476+ChillitheCode@users.noreply.github.com>
2019-11-04 15:43:41 +0000

    Update CCF.py
    The theory works however some replies seem to be lost.

--- ChilliConversationalFlow.py
+++ ChilliConversationalFlow.py
@@ -1,18 +1,18 @@
 import discord
 import random
-#from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
+from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
 from googletrans import Translator
 from sportsreference.nba.teams import Teams
 #from Trivia import quiz
 import random
 
 client = discord.Client()
-#analyzer = SentimentIntensityAnalyzer()
+analyzer = SentimentIntensityAnalyzer()
 translator = Translator()
 
-#@client.event
-#async def on_ready():
-#    print("We have logged in") 
+@client.event
+async def on_ready():
+    print("We have logged in") 
 
 @client.event
 async def on_ready():
@@ -20,87 +20,86 @@ async def on_ready():
 
 
 def get_username(message):
-    
+    username = ""
     if message.author == client.user:
         return
     else:
-        user = message.member
+        user = str(message.author)
     if "!" in user:
         username = user[1:]
         return username
-    elif "@" in user:
-        username = user[1:]
+    else:
         return username
         
 
-#def sentiment_analyzer_scores(text):
-#    trans = translator.translate(text).text
+def sentiment_analyzer_scores(text):
+    trans = translator.translate(text).text
 
-#    score = analyzer.polarity_scores(trans)
-#    lb = score['compound']
-#    if lb >= 0.05:
-#        return 'positive'
-#    elif (lb > -0.05) and (lb < 0.05):
-#        return 'neutral'
-#    else:
-#        return 'negative'
+    score = analyzer.polarity_scores(trans)
+    lb = score['compound']
+    if lb >= 0.05:
+        return 'positive'
+    elif (lb > -0.05) and (lb < 0.05):
+        return 'neutral'
+    else:
+        return 'negative'
 
 @client.event
-async def on_message_Hi(message):
-    messagetext = str(message.content.upper)
-    if messagetext in message.content.upper:
-        MSG="Howdy and welcome to the Stephen A. Smith Show"
-        await message.channel.send(MSG)
-        #sentiment = sentiment_analyzer_scores(message.content)
-        #await message.channel.send('The sentiment of your text is ' + str(sentiment))
-        #get_username(message)
-        #if sentiment == 'negative':
-        #    await mesage.channel.send('The bot responds with a mean statement.')
-        #    on_message_responses(sentiment, username)
-        #else:
-        #    await message.channel.send('The bot responds with a nice statement.')
-        #    on_message_responses(sentiment, username)
-    
+async def on_message(message):
+    username = get_username(message)
+    intro = False
+    if intro == False:
+        if "HI" in message.content.upper():
+            intro = True
+            MSG = on_message_Hi(intro)
+            await message.channel.send(MSG)
+    elif intro == True:
+        text = Rant()
+        await message.channel.send(text)
+        if "TRIVIA" in message.content.upper():
+            await message.channel.send('U ready for a lil basketball trivia?')
+            on_message_trivia(message, username)
     if message.author == client.user:
         return
-    
-    #sentiment = sentiment_analyzer_scores(message.content)
-    #await message.channel.send('The sentiment of your text is ' + str(sentiment))
-    #get_username(message)
-    #if sentiment == 'negative':
-    #    await mesage.channel.send('The bot responds with a mean statement.')
-    #    on_message_responses(sentiment, username)
-    #else:
-    #    await message.channel.send('The bot responds with a nice statement.')
-    #    on_message_responses(sentiment, username)
+    sentiment = sentiment_analyzer_scores(message.content)
+    await message.channel.send('The sentiment of your text is ' + str(sentiment))
+    get_username(message)
+    if sentiment == 'negative':
+        await message.channel.send('The bot responds with a mean statement.')
+        reply = on_message_responses(sentiment, username)
+        await message.channel.send(reply)
+    else:
+        await message.channel.send('The bot responds with a nice statement.')
+        reply = on_message_responses(sentiment, username)
+        await message.channel.send(reply)
+
+def Rant():
+  ToSay = ["that's preposterous. The Warriors' start, things of that nature. Golden State is 0-2 for the first time in six seasons under coach Steve Kerr, with 19- and 28-point losses on its ledger and the NBA's worst defensive rating (124.3) as of Monday morning.  The Warriors have been a far cry from their dynastic selves, and the ESPN commentator blasted the Dubs on Monday. You know he has respect for Kerr, Steph Curry and Draymond Green. Absolutely.",
+           "You don't get to sit back and go like this, 'Well, this is what you can expect from us.' No, the hell we won't expect that! You have enough to fight! That's all I'm saying! You can lose, fade in the fourth quarter. Fade because you're an inferior team, talent-wise. Fade because you don't have the same level of talent that others have, but to come out there and to just get straight punked on a Sunday afternoon by 40 in Oklahoma City? That's disgraceful! That's disgraceful!",
+           "It’s my house… I OWN IT. I don’t own you, I own it! But I own it. The idiocy in this politically correct world that we’re living in, where we gotta literally have a discussion to the point it becomes a media storyline. Oh my God, an owner says he owns his team, that’s offensive to people. Y’all smoking crack.",
+           "Don't give a damn what anyone says: weak move by KD. You go to GSW, the team who beat you, when you're already on a title contender? Please!"
+           ]
+  text = random.choice(ToSay)
+  return text
+  
+
+#def sentiment_response(message):
+
+
+def on_message_Hi(intro):
+    MSGS=["HELLO!","Howdy", "Welcome to the Stephen B. Smith Show", "This is Good Burger home of the good burger can i take your order"]
+    MSG = random.choice(MSGS)
+    return MSG
 
 
 def on_message_Trivia(message, username):
-  message.channel.send('U ready for a lil basketball trivia?')
-  #sentiment = sentiment_analyzer_scores(message.content)
-  #if sentiment == 'negative':
-  #  message.channel.send('The bot responds with a mean statement.')
-  #  on_message_responses(sentiment, username)
-  #else:
-  #  message.channel.send('The bot responds with a nice statement.')
-  #  on_message_responses(sentiment, username)
-    #quiz(username)
+    sentiment = sentiment_analyzer_scores(message.content)
+    if sentiment == 'negative':
+        return
+    else:
+        quiz(username)
+        return
  
-
-def on_message_MatchFacts(message, username):
-    FavTeam = GetFavTeam(username)
-    message.channel.send("Definitely not VAR's finest performance, you want the latest match scores and highlights for " + FavTeam + "?")
-    #if sentiment == 'negative':
-    #    mesage.channel.send('The bot responds with a mean statement.')
-    #    on_message_responses(sentiment, username)
-    #    message.channel.send('Do you want the stats yes or no? \n{{{you must reply  with yes/no}}}\n ')
-    #    if "YES" in message.content.upper():
-    #        message.channel.send("The game facts")
-    #        GetGamefacts(FavTeam)
-    #    else:
-    #        message.channel.send('The bot responds with a nice statement.')
-    #        message.channel.send("The game facts")
-    #        GetGamefacts(FavTeam)
 
 def on_message_responses(sentiment, username):
     UncenNegWB  = ["Oooooh, that shit is blasphemous", "Shiiieeeeet, I've been covering the NBA for 25 years. I've NEVER been more insulted", "Who does this bitch and yes i said BITCH think they are!"]
@@ -109,17 +108,21 @@ def on_message_responses(sentiment, username):
     CenPosWB = ["Obviously we're having a very GOOD day", "See this, this is called chemistry", "I love you"]
     accountType = GetAccountType(username)
     if sentiment == 'negative' and accountType == 'uncensored':
-        message.channel.send(random.choice(UncenNegWB))
+        reply = random.choice(UncenNegWB)
+        return reply
     elif sentiment == 'postive' and accountType == 'uncensored':
-        message.channel.send(random.choice(UncenPosWB))
+        reply = random.choice(UncenPosWB)
+        return reply
     elif sentiment == 'postive' and accountType == 'censored':
-        message.channel.send(random.choice(CenPosWB))
+        reply = random.choice(CenPosWB)
+        return reply
     elif sentiment == 'negtive' and accountType == 'censored':
-        message.channel.send(random.choice(CenNegWB))
+        reply = random.choice(CenNegWB)
+        return reply
  
 
-def GetGameStats(FavTeam):
-    message.channel.send('Match facts and stats')
+#def GetGameStats(FavTeam):
+#    message.channel.send('Match facts and stats')
 
 #@client.event
 #async def on_message_swearing(message):
