8eccf16809251c959a18859ab3452a2906972b22
ChillitheCode <56298476+ChillitheCode@users.noreply.github.com>
2019-11-10 20:06:14 +0000

    Update ChilliConversationalFlow.py

--- ChilliConversationalFlow.py
+++ ChilliConversationalFlow.py
@@ -1,8 +1,4 @@
-import discord
-import random
-from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
-from googletrans import Translator
-from sportsreference.nba.teams import Teams
+#This file has been copyrighted, plagarism and missuse will not be torelated.
 #from Trivia import quiz
 import random
 
@@ -10,13 +6,13 @@ client = discord.Client()
 analyzer = SentimentIntensityAnalyzer()
 translator = Translator()
 
-@client.event
-async def on_ready():
-    print("We have logged in") 
+#@client.event
+#async def on_ready():
+#    print("We have logged in") 
 
 @client.event
 async def on_ready():
-  print('We have logged in as {0.user}'.format(client))
+    print('We have logged in as {0.user}'.format(client))
 
 
 def get_username(message):
@@ -46,31 +42,48 @@ def sentiment_analyzer_scores(text):
 
 @client.event
 async def on_message(message):
-    username = get_username(message)
-    intro = False
-    while intro==False:
-        if "HI" in message.content.upper():
-            intro = True
-            MSG = on_message_Hi(intro)
-            await message.channel.send(MSG)
-     text = Rant()
-     await message.channel.send(text)
-     if "TRIVIA" in message.content.upper():
-        await message.channel.send('U ready for a lil basketball trivia?')
-        on_message_trivia(message, username)
     if message.author == client.user:
         return
+    
+    accountType = 'cen'
+    online = True
+    username = get_username(message)
+    #while intro==False:
+    if "HI" in message.content.upper():
+        intro = True
+        MSG = on_message_Hi(intro)
+        await message.channel.send(MSG)
+    
+    if "TRIVIA" in message.content.upper():
+        await message.channel.send('U ready for a lil basketball trivia?')
+        on_message_trivia(message, username)
+    
+    if message.author == client.user:
+        return
+    
     sentiment = sentiment_analyzer_scores(message.content)
     await message.channel.send('The sentiment of your text is ' + str(sentiment))
-    get_username(message)
     if sentiment == 'negative':
         await message.channel.send('The bot responds with a mean statement.')
-        reply = on_message_responses(sentiment, username)
+        for n in ["SHIT","FUCK","BITCH"]:
+            if n in message.content.upper():
+                accountType = on_message_swearing()
+        reply = on_message_responses(sentiment, accountType)
         await message.channel.send(reply)
     else:
         await message.channel.send('The bot responds with a nice statement.')
-        reply = on_message_responses(sentiment, username)
+        for n in ["SHIT","FUCK","BITCH"]:
+            if n in message.content.upper():
+                accountType = on_message_swearing()
+        reply = on_message_responses(sentiment, accountType)
         await message.channel.send(reply)
+
+    Atext = ['Ooooo I have something to say...','Lemme talk about basketbaaalll real quick,...']
+    await message.channel.send(random.choice(Atext))
+    text = Rant()
+    await message.channel.send(text)
+    Stext = '...anyway, what can i do for you?'
+    await message.channel.send(Stext)
 
 def Rant():
   ToSay = ["that's preposterous. The Warriors' start, things of that nature. Golden State is 0-2 for the first time in six seasons under coach Steve Kerr, with 19- and 28-point losses on its ledger and the NBA's worst defensive rating (124.3) as of Monday morning.  The Warriors have been a far cry from their dynastic selves, and the ESPN commentator blasted the Dubs on Monday. You know he has respect for Kerr, Steph Curry and Draymond Green. Absolutely.",
@@ -100,33 +113,33 @@ def on_message_Trivia(message, username):
         return
  
 
-def on_message_responses(sentiment, username):
+def on_message_responses(sentiment, accountType):
     UncenNegWB  = ["Oooooh, that shit is blasphemous", "Shiiieeeeet, I've been covering the NBA for 25 years. I've NEVER been more insulted", "Who does this bitch and yes i said BITCH think they are!"]
     CenNegWB = ["BLASPHEMOUS!!!", "Another day...another nauseating fan", "What in the HELL are you getting cute for..FOR WHAT!!","I reckon someone could use a hug", "Only weed would do this, you must be high.", "Stay off the  WEEEEED"]
     UncenPosWB = ["Obviously we're having a very GOOD fucking day", "See this, this shit is called chemistry", "Lets GOOOO"]
-    CenPosWB = ["Obviously we're having a very GOOD day", "See this, this is called chemistry", "I love you"]
-    accountType = GetAccountType(username)
-    if sentiment == 'negative' and accountType == 'uncensored':
-        reply = random.choice(UncenNegWB)
-        return reply
-    elif sentiment == 'postive' and accountType == 'uncensored':
-        reply = random.choice(UncenPosWB)
-        return reply
-    elif sentiment == 'postive' and accountType == 'censored':
-        reply = random.choice(CenPosWB)
-        return reply
-    elif sentiment == 'negtive' and accountType == 'censored':
-        reply = random.choice(CenNegWB)
-        return reply
+    CenPosWB = ["Obviously we're having a very GOOD day", "See this, this is called chemistry", "I love your energy"]
+    
+    if sentiment == 'negative' and accountType == 'uncen':
+        return random.choice(UncenNegWB)
+
+    elif sentiment == 'neutral' and accountType == 'uncen':
+        return random.choice(UncenPosWB)
+    
+    elif sentiment == 'neutral' and accountType == 'cen':
+        return random.choice(CenPosWB)
+    
+    elif sentiment == 'negtive' and accountType == 'cen':
+        return random.choice(CenNegWB)
  
 
 #def GetGameStats(FavTeam):
 #    message.channel.send('Match facts and stats')
 
-#@client.event
-#async def on_message_swearing(message):
-#    print
+
+def on_message_swearing():
+    accountType = 'uncen'
+    return accountType
 
     
         
-client.run('token')
+client.run('Token')
